393843dab12b27648e83109c2ffca3debef48607
ELIELBAIA <132669531+ElielBaia@users.noreply.github.com>
2026-08-07 12:50:54 -0300

    Add BABEL Android activity

---
+++ babel-ci/project/app/src/main/java/com/forsetigames/babel/MainActivity.java
@@ -0,0 +1,48 @@
+package com.forsetigames.babel;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+
+public final class MainActivity extends Activity {
+    private GameView gameView;
+
+    @Override public void onCreate(Bundle state) {
+        super.onCreate(state);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+        hideSystemUi();
+        gameView = new GameView(this);
+        setContentView(gameView);
+    }
+
+    private void hideSystemUi() {
+        getWindow().getDecorView().setSystemUiVisibility(
+                View.SYSTEM_UI_FLAG_FULLSCREEN |
+                View.SYSTEM_UI_FLAG_HIDE_NAVIGATION |
+                View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY |
+                View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN |
+                View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION |
+                View.SYSTEM_UI_FLAG_LAYOUT_STABLE);
+    }
+
+    @Override public void onWindowFocusChanged(boolean hasFocus) {
+        super.onWindowFocusChanged(hasFocus);
+        if (hasFocus) hideSystemUi();
+    }
+
+    @Override protected void onPause() {
+        super.onPause();
+        if (gameView != null) gameView.onHostPause();
+    }
+
+    @Override protected void onResume() {
+        super.onResume();
+        hideSystemUi();
+        if (gameView != null) gameView.onHostResume();
+    }
+}
